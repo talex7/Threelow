f83d0f7e6ab7d2a61b8687559621bd85733ed132
talex7 <talexanian@gmail.com>
2016-11-02 20:24:22 -0400

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Thomas Alexanian on 2016-11-02.
+//  Copyright © 2016 Thomas Alexanian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
